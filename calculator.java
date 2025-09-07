a9f6e851f1cc1ba8b54b532fd093982ac38e21bc
Ahmed <am1225392@gmail.com>
2025-09-07 12:18:00 +0300

    Initial commit with Calculator.java and README.md

---
+++ calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
+
