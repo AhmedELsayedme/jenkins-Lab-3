29860904773e5e7e99dd701a71958304e336c5bc
Ahmed <am1225392@gmail.com>
2025-09-07 12:35:35 +0300

    this is updated java

--- calculator.java
+++ calculator.java
@@ -1,9 +1,12 @@
 public class Calculator {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 7;
-        int sum = a + b;
-        System.out.println("Sum = " + sum);
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
     }
 }
 
